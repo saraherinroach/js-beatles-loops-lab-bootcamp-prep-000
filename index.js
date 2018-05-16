27efc9c5e2a792844fd22b7a4cce518ccd143eaf
Sarah Erin Roach <saraherinroach@gmail.com>
2018-05-16 21:17:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ function theBeatlesPlay (musician,instrument) {
     theBeatlesPlay[i]=`${musician[i]} plays ${instrument[i]}`
   }
   return theBeatlesPlay
-  }
+}
   
-  function johnLennonFacts (facts) {
-    var countdown = facts.length;
-    var i = 0;
-    while (countdown>0) {
-      facts[i] = `${facts[i]}!!!`;
-      i++;
-      --countdown;
-    }
-    return facts
+function johnLennonFacts (facts) {
+  var countdown = facts.length;
+  var i = 0;
+  while (countdown>0) {
+    facts[i] = `${facts[i]}!!!`;
+    i++;
+    --countdown;
   }
+  return facts
+}
+
