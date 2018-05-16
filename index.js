bec8a20c80fcb664647b1923726d0f0d437ba7dd
Sarah Erin Roach <saraherinroach@gmail.com>
2018-05-16 20:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay (musician,instrument) {
   var theBeatlesPlay = [];
   for (var i = 0;i<4;i++) {
     theBeatlesPlay[[i]]=`${musician[i]} plays ${instrument[i]}`
-  return theBeatlesPlay
   }
+  return theBeatlesPlay
   }
   
